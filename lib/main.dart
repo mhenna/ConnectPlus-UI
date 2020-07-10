4921d3877749fb3a52f7d4c9ff3ac0013f3b6836
Mohamed Yasser <mohamedyasser96@aucegypt.edu>
2020-07-10 21:40:47 +0200

    Function the side nav bar, also added bottom nav bar for preview with dummy page routing

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:connect_plus/SideNav.dart';
+import 'package:connect_plus/Navbar.dart';
 
 void main() {
   runApp(MyApp());
@@ -78,39 +79,7 @@ class _MyHomePageState extends State<MyHomePage> {
         // the App.build method, and use it to set our appbar title.
         title: Text(widget.title),
       ),
-      drawer: Drawer(
-        child: ListView(
-          // Important: Remove any padding from the ListView.
-          padding: EdgeInsets.zero,
-          children: <Widget>[
-            Container(
-              height: 100.0,
-              child: DrawerHeader(
-                child: Image.network(
-                  'http://www.emc2movecar.com/connectplus/wp-content/uploads/2018/01/logo.png',
-                ),
-                decoration: BoxDecoration(
-                  color: Colors.blue,
-                ),
-              ),
-            ),
-            ListTile(
-              title: Text('Item 1'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-            ListTile(
-              title: Text('Item 2'),
-              onTap: () {
-                // Update the state of the app.
-                // ...
-              },
-            ),
-          ],
-        ),
-      ),
+      drawer: NavDrawer(),
       body: Center(
         // Center is a layout widget. It takes a single child and positions it
         // in the middle of the parent.
