2e86afbd579f2eed321f8d2b4c801434677a5826
hennam <mostafa.henna@emc.com>
2020-09-01 21:52:28 +0200

    implemented auto login

--- lib/login.dart
+++ lib/login.dart
@@ -7,6 +7,7 @@ import 'package:password/password.dart';
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class login extends StatefulWidget {
   login({Key key, this.title}) : super(key: key);
@@ -24,12 +25,15 @@ class _loginState extends State<login> {
   final pwController = TextEditingController();
   final algorithm = PBKDF2();
   TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+  SharedPreferences prefs;
   var ip;
   var port;
+  var loading = true;
 
   void initState() {
     super.initState();
     setEnv();
+    checkLoggedInStatus();
   }
 
   setEnv() {
@@ -42,132 +46,159 @@ class _loginState extends State<login> {
     return hash;
   }
 
+  void checkLoggedInStatus() async {
+    prefs = await SharedPreferences.getInstance();
+    var url = 'http://' + ip + ':' + port + '/user/validate';
+    var token = prefs.getString("token");
+
+    var response = await http.get(url, headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer $token"
+    });
+
+    if (response.statusCode == 200) {
+      loading = false;
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => MyHomePage()),
+      );
+    } else {
+      setState(() {
+        loading = false;
+      });
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    final emailField = TextField(
-      controller: emController,
-      obscureText: false,
-      style: style,
-      decoration: InputDecoration(
-          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "Email(@dell.com)",
-          border:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(15.0))),
-    );
-    final passwordField = TextField(
-      controller: pwController,
-      obscureText: true,
-      style: style,
-      decoration: InputDecoration(
-          contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-          hintText: "Password",
-          border:
-              OutlineInputBorder(borderRadius: BorderRadius.circular(15.0))),
-    );
-    final loginTitle = Text.rich(
-      TextSpan(children: <TextSpan>[
-        TextSpan(
-            text: ' Login ',
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Color(0xfff7501e),
-                fontSize: 30.0,
-                fontFamily: "Arial"))
-      ]),
-    );
-    final register = Text.rich(
-      TextSpan(children: <TextSpan>[
-        TextSpan(
-            text: ' Not a user? ',
-            style: TextStyle(
-                color: Colors.black, fontSize: 15.0, fontFamily: "Arial")),
-        TextSpan(
-            text: ' Register now ',
-            style: TextStyle(
-                fontWeight: FontWeight.bold,
-                color: Color(0xfff7501e),
-                fontSize: 15.0,
-                fontFamily: "Arial"),
-            recognizer: TapGestureRecognizer()
-              ..onTap = () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => registration()),
-                );
-              })
-      ]),
-    );
-    final loginButton = Material(
-      elevation: 5.0,
-      borderRadius: BorderRadius.circular(30.0),
-      color: Color(0xFFE15F5F),
-      child: MaterialButton(
-        minWidth: MediaQuery.of(context).size.width,
-        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
-        onPressed: () {
-          loginPressed();
-        },
-        child: Text("Login",
-            textAlign: TextAlign.center,
-            style: style.copyWith(
-                color: Colors.white, fontWeight: FontWeight.bold)),
-      ),
-    );
-
-    return Scaffold(
-      body: Center(
-        child: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.only(top: 10.0),
-            child: Container(
-              decoration: BoxDecoration(
-                color: Color(0xfffafafa),
-                image: DecorationImage(
-                  image: AssetImage("assets/logo2.png"),
-                  fit: BoxFit.fitWidth,
-                  alignment: Alignment.topCenter,
+    if (loading)
+      return LoadingWidget();
+    else {
+      final emailField = TextField(
+        controller: emController,
+        obscureText: false,
+        style: style,
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            hintText: "Email(@dell.com)",
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(15.0))),
+      );
+      final passwordField = TextField(
+        controller: pwController,
+        obscureText: true,
+        style: style,
+        decoration: InputDecoration(
+            contentPadding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+            hintText: "Password",
+            border:
+                OutlineInputBorder(borderRadius: BorderRadius.circular(15.0))),
+      );
+      final loginTitle = Text.rich(
+        TextSpan(children: <TextSpan>[
+          TextSpan(
+              text: ' Login ',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xfff7501e),
+                  fontSize: 30.0,
+                  fontFamily: "Arial"))
+        ]),
+      );
+      final register = Text.rich(
+        TextSpan(children: <TextSpan>[
+          TextSpan(
+              text: ' Not a user? ',
+              style: TextStyle(
+                  color: Colors.black, fontSize: 15.0, fontFamily: "Arial")),
+          TextSpan(
+              text: ' Register now ',
+              style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  color: Color(0xfff7501e),
+                  fontSize: 15.0,
+                  fontFamily: "Arial"),
+              recognizer: TapGestureRecognizer()
+                ..onTap = () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (context) => registration()),
+                  );
+                })
+        ]),
+      );
+      final loginButton = Material(
+        elevation: 5.0,
+        borderRadius: BorderRadius.circular(30.0),
+        color: Color(0xFFE15F5F),
+        child: MaterialButton(
+          minWidth: MediaQuery.of(context).size.width,
+          padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+          onPressed: () {
+            loginPressed();
+          },
+          child: Text("Login",
+              textAlign: TextAlign.center,
+              style: style.copyWith(
+                  color: Colors.white, fontWeight: FontWeight.bold)),
+        ),
+      );
+      return Scaffold(
+        body: Center(
+          child: SingleChildScrollView(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 10.0),
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0xfffafafa),
+                  image: DecorationImage(
+                    image: AssetImage("assets/logo2.png"),
+                    fit: BoxFit.fitWidth,
+                    alignment: Alignment.topCenter,
+                  ),
                 ),
-              ),
-              child: Column(
-                children: [
-                  Padding(
-                    padding: const EdgeInsets.fromLTRB(36.0, 220.0, 36.0, 30.0),
-                    child: Card(
-                      child: Column(
-                        children: <Widget>[
-                          SizedBox(height: 20.0),
-                          Container(
-                              alignment: Alignment.centerLeft,
-                              child: loginTitle),
-                          SizedBox(height: 20.0),
-                          Container(
-                            width: 250,
-                            child: emailField,
-                          ),
-                          SizedBox(height: 20.0),
-                          Container(
-                            width: 250,
-                            child: passwordField,
-                          ),
-                          SizedBox(height: 20.0),
-                          Container(
-                            width: 250,
-                            child: Padding(
-                                padding: EdgeInsets.only(bottom: 20),
-                                child: loginButton),
-                          ),
-                        ],
+                child: Column(
+                  children: [
+                    Padding(
+                      padding:
+                          const EdgeInsets.fromLTRB(36.0, 220.0, 36.0, 30.0),
+                      child: Card(
+                        child: Column(
+                          children: <Widget>[
+                            SizedBox(height: 20.0),
+                            Container(
+                                alignment: Alignment.centerLeft,
+                                child: loginTitle),
+                            SizedBox(height: 20.0),
+                            Container(
+                              width: 250,
+                              child: emailField,
+                            ),
+                            SizedBox(height: 20.0),
+                            Container(
+                              width: 250,
+                              child: passwordField,
+                            ),
+                            SizedBox(height: 20.0),
+                            Container(
+                              width: 250,
+                              child: Padding(
+                                  padding: EdgeInsets.only(bottom: 20),
+                                  child: loginButton),
+                            ),
+                          ],
+                        ),
                       ),
                     ),
-                  ),
-                  register
-                ],
+                    register
+                  ],
+                ),
               ),
             ),
           ),
         ),
-      ),
-    );
+      );
+    }
   }
 
   void loginPressed() async {
@@ -181,6 +212,7 @@ class _loginState extends State<login> {
         headers: {"Content-Type": "application/json"}, body: msg);
     if (response.statusCode == 200) {
       localStorage.setItem("token", json.decode(response.body)["token"]);
+      prefs.setString("token", json.decode(response.body)["token"]);
       Navigator.push(
         context,
         MaterialPageRoute(builder: (context) => MyHomePage()),
@@ -213,5 +245,18 @@ class _loginState extends State<login> {
         );
       },
     );
+  }
+
+  Widget LoadingWidget() {
+    return Scaffold(
+        body: Center(
+      child: Row(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          new CircularProgressIndicator(),
+          new Text("Loading"),
+        ],
+      ),
+    ));
   }
 }
