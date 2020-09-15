3e3eb3e12cb64a7dfc962296b28e9982baf222c4
v1ll41n <kareem.m.selim@gmail.com>
2020-09-15 12:19:21 +0200

    added activities to calendar

--- lib/Calendar.dart
+++ lib/Calendar.dart
@@ -5,7 +5,6 @@ import 'package:http/http.dart' as http;
 import 'package:localstorage/localstorage.dart';
 import 'dart:convert';
 import 'package:connect_plus/Navbar.dart';
-import 'package:connect_plus/Event.dart';
 
 class Calendar extends StatefulWidget {
   @override
@@ -15,6 +14,8 @@ class Calendar extends StatefulWidget {
 class _CalendarState extends State<Calendar> {
   CalendarController _controller;
   Map<DateTime, List<dynamic>> _events;
+  Map<DateTime, List<dynamic>> _activities;
+  Map<DateTime, List<dynamic>> _all;
   List<dynamic> _selectedEvents;
   var ip;
   var port;
@@ -24,10 +25,13 @@ class _CalendarState extends State<Calendar> {
   void initState() {
     super.initState();
     _events = {};
+    _activities = {};
+    _all={};
     _selectedEvents = [];
     _controller = CalendarController();
     setEnv();
     getEvents();
+    getActivities();
   }
 
   setEnv() {
@@ -52,7 +56,62 @@ class _CalendarState extends State<Calendar> {
         else
           _events[date] = [event];
       }
+    _all.addAll(_events);
     }
+
+  }
+
+void getActivities() async {
+    var activities;
+    String token = localStorage.getItem("token");
+    var url = 'http://' + ip + ':' + port + '/activity';
+    var response = await http.get(url, headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer $token"
+    });
+    if (response.statusCode == 200) {
+      activities = json.decode(response.body);
+      for (var activity in activities) {
+        var dates = activity["recurrenceDates"];
+        for (var date in dates)
+        {
+           date = DateTime.parse(date);
+           if (_activities[date] != null)
+                _activities[date].add(activity);
+          else
+             _activities[date] = [activity];
+         }
+      }
+      _all.addAll(_activities);
+    }
+  }
+
+
+  Widget _buildEventsMarker(DateTime date, List events) {
+     for (var item in events) {
+        bool condition= _events.containsKey(DateTime.parse(item['startDate']));
+        return AnimatedContainer(
+         duration: const Duration(milliseconds: 300),
+         decoration: BoxDecoration(
+          shape:  condition ? BoxShape.circle : BoxShape.rectangle,
+          color:  _controller.isSelected(date)? (condition ? Colors.brown[500] : Colors.blue[500])
+                 : _controller.isToday(date) ?  (condition ? Colors.red[300] : Colors.yellow[400]) 
+                 : (condition ? Colors.black : Colors.green[300]) 
+        ),
+         width: 16.0,  
+        height: 16.0,
+        child: Center(
+         child: Text(
+          '${events.length}',
+          style: TextStyle().copyWith(
+            color: Colors.white,
+            fontSize: 12.0,
+          ),
+        ),
+      ),
+    );
+       }
+
   }
 
   @override
@@ -67,43 +126,35 @@ class _CalendarState extends State<Calendar> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
               TableCalendar(
-                events: _events,
+                events: _all,
                 calendarController: _controller,
-                calendarStyle: CalendarStyle(
-                  todayColor: Color(0xFFE15F5F),
-                  selectedColor: Colors.black,
-                ),
                 weekendDays: [5, 6],
                 onDaySelected: (date, events) {
                   setState(() {
                     _selectedEvents = events;
                   });
                 },
-                builders: CalendarBuilders(
-                  singleMarkerBuilder: (context, date, event) {
-                    Color cor = Color(int.parse(event["ERG"]["color"]));
-                    return Container(
-                      decoration:
-                          BoxDecoration(shape: BoxShape.circle, color: cor),
-                      width: 7.0,
-                      height: 7.0,
-                      margin: const EdgeInsets.symmetric(horizontal: 1.5),
-                    );
-                  },
-                ),
+              builders: CalendarBuilders(
+                markersBuilder: (context, date, events, holidays) {
+                final children = <Widget>[];
+                if (events.isNotEmpty) {
+                children.add(
+                  Positioned(
+                   right: 1,
+                   bottom: 1,
+                   child: _buildEventsMarker(date, events),
+                  ),
+                );
+              }
+              return children;
+           },
+          ),
+                
               ),
+
               ..._selectedEvents.map((event) => ListTile(
-                  title: Text(event["name"]),
-                  onTap: () {
-                    Navigator.push(
-                      context,
-                      MaterialPageRoute(
-                          builder: (context) => Event(
-                                event: event["name"],
-                                erg: event['ERG']["name"],
-                              )),
-                    );
-                  })),
+                    title: Text(event["name"]),
+                  )),
             ])));
   }
 }
