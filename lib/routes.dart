3e51c063e74e45771df4cde39fbdad82750f4844
Rawan A. El Ashry <rawanelashry@gmail.com>
2020-09-26 13:17:59 +0200

    Push Notifications IOS

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,3 +1,4 @@
+import 'package:connect_plus/Activities.dart';
 import 'package:flutter/material.dart';
 import 'Events.dart';
 import 'Offers.dart';
@@ -7,11 +8,13 @@ class Routes{
 
   static const String events = '/events';
   static const String offers = '/offers';
+  static const String activities = '/activities';
 
   static final routes = <String, WidgetBuilder>{
 
     events: (BuildContext context) => Events(),
     offers: (BuildContext context) => Offers(),
+    activities: (BuildContext context) => Activities(),
 
   };
 }
