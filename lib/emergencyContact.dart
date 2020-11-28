9bad0d1ae9d180daf04ff956f5f7495cd229e0e2
Rawan A. El Ashry <rawanelashry@gmail.com>
2020-11-28 22:10:25 +0200

    Refactored Emergency Contacts to be dynamic

--- lib/emergencyContact.dart
+++ lib/emergencyContact.dart
@@ -1,153 +1,125 @@
+import 'package:connect_plus/models/emergencyContact.dart';
+import 'package:connect_plus/services/web_api.dart';
+import 'package:connect_plus/widgets/ImageRotate.dart';
 import 'package:connect_plus/widgets/Utils.dart';
 import 'package:flutter/material.dart';
-import 'package:connect_plus/widgets/app_scaffold.dart';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:connect_plus/Navbar.dart';
 
-class emergencyContact extends StatelessWidget {
+class Emergency extends StatefulWidget {
+  Emergency({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  MyEmergencyPageState createState() => MyEmergencyPageState();
+}
+
+class MyEmergencyPageState extends State<Emergency>
+    with AutomaticKeepAliveClientMixin<Emergency> {
+  @override
+  bool get wantKeepAlive => true;
+  List<EmergencyContact> contacts;
+  List<Widget> bupa = new List<Widget>();
+  List<Widget> dell = new List<Widget>();
+  List<Widget> local = new List<Widget>();
+
+  void initState() {
+    contacts = [];
+    bupa = [];
+    dell = [];
+    local = [];
+    super.initState();
+    getContacts();
+  }
+
+  void getContacts() async {
+    final allContacts = await WebAPI.getEmergencyContacts();
+    if (this.mounted) {
+      setState(() {
+        contacts = allContacts;
+      });
+      distributeContacts();
+    }
+  }
+
+  void distributeContacts() {
+    contacts.forEach((contact) {
+      if (contact.type == 'BUPA') {
+        bupa.add(
+          ListTile(
+            title: Text(contact.name, style: TextStyle(color: Utils.header)),
+            onTap: () => launch(contact.number),
+          ),
+        );
+      } else if (contact.type == 'DELL') {
+        dell.add(
+          ListTile(
+            title: Text(contact.name, style: TextStyle(color: Utils.header)),
+            onTap: () => launch(contact.number),
+          ),
+        );
+      } else {
+        local.add(
+          ListTile(
+            title: Text(contact.name, style: TextStyle(color: Utils.header)),
+            onTap: () => launch(contact.number),
+          ),
+        );
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     var width = MediaQuery.of(context).size.width;
     var height = MediaQuery.of(context).size.height;
-    return DefaultTabController(
-        length: 3,
-        child: new Scaffold(
-          drawer: NavDrawer(),
-          appBar: AppBar(
-            centerTitle: true,
-            flexibleSpace: Container(
-              decoration: BoxDecoration(
-                gradient: LinearGradient(
-                  colors: [
-                    Utils.secondaryColor,
-                    Utils.primaryColor,
-                  ],
-                  begin: Alignment.topRight,
-                  end: Alignment.bottomLeft,
+    if (bupa.isNotEmpty || dell.isNotEmpty || local.isNotEmpty)
+      return DefaultTabController(
+          length: 3,
+          child: new Scaffold(
+            drawer: NavDrawer(),
+            appBar: AppBar(
+              centerTitle: true,
+              flexibleSpace: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      Utils.secondaryColor,
+                      Utils.primaryColor,
+                    ],
+                    begin: Alignment.topRight,
+                    end: Alignment.bottomLeft,
+                  ),
                 ),
               ),
+              title: Text('Emergency Contacts'),
+              bottom: TabBar(tabs: [
+                Tab(
+                  text: "BUPA",
+                ),
+                Tab(
+                  text: "Dell",
+                ),
+                Tab(
+                  text: "Local",
+                ),
+              ]),
             ),
-            title: Text('Emergency Contacts'),
-            bottom: TabBar(tabs: [
-              Tab(
-//              icon: Icon(Icons.local_hospital),
-                text: "BUPA",
-              ),
-              Tab(
-                //icon: Icon(Icons.home),
-                text: "Dell",
-              ),
-              Tab(
-                text: "Local",
-              ),
-            ]),
-          ),
-          body: Padding(
-            padding: EdgeInsets.fromLTRB(
-                width * 0.02, height * 0.02, width * 0.03, height * 0.01),
-            child: TabBarView(children: [
-              new ListView(
-                children: <Widget>[
-                  ListTile(
-                    title: Text('BUPA Egypt Hotline',
-                        style: TextStyle(color: Utils.header)),
-                    onTap: () => launch("tel://16816"),
-                  ),
-                  ListTile(
-                    title: Text('Bupa Medical queries (Outside Egypt)',
-                        style: TextStyle(color: Utils.header)),
-                    onTap: () => launch("tel:+202 27681100"),
-                  ),
-                  ListTile(
-                    title: Text('BUPA Emergency contact',
-                        style: TextStyle(color: Utils.header)),
-                    onTap: () => launch("tel:+201227624595"),
-                  ),
-                  ListTile(
-                    title: Text('BUPA Medical Center',
-                        style: TextStyle(color: Utils.header)),
-                    onTap: () => launch("tel: +44 1273333911"),
-                  ),
-                  ListTile(
-                    title: Text('BUPA 24/7 Hotline',
-                        style: TextStyle(color: Utils.header)),
-                    onTap: () => launch("tel: +44 1273 323 563"),
-                  ),
-                  ListTile(
-                    title: Text('Travel Security Emergency (ISOS)',
-                        style: TextStyle(color: Utils.header)),
-                    onTap: () => launch("tel: +44 20 7939 8899"),
-                  ),
-                  ListTile(
-                    title: Text('*Rehab Emergency Medical Center',
-                        style: TextStyle(color: Utils.header)),
-                    onTap: () => launch("tel: +202 26077980"),
-                  ),
-                  ListTile(
-                    title: Text('*Air Force Specialized Hospital',
-                        style: TextStyle(color: Utils.header)),
-                    onTap: () => launch("tel://19448"),
-                  )
-                ],
-              ),
-              new ListView(
-                children: <Widget>[
-                  ListTile(
-                    title: Text('DELL EMC Office',
-                        style: TextStyle(color: Utils.header)),
-                    onTap: () => launch("tel:+202 2 5032500"),
-                  ),
-                  ListTile(
-                    title: Text('DELL EMC Fax',
-                        style: TextStyle(color: Utils.header)),
-                    onTap: () => launch("tel: +202 2503 2555"),
-                  ),
-                  ListTile(
-                    title: Text('IT Global Desk: +1 800-782-3797 opt 1 x58126'),
-                  ),
-                  ListTile(
-                    title: Text('IT International Direct',
-                        style: TextStyle(color: Utils.header)),
-                    onTap: () => launch("tel: +1 (508) 898-5812"),
-                  )
-                ],
-              ),
-              new ListView(
-                children: <Widget>[
-                  ListTile(
-                    title: Text('Ambulance – 123',
-                        style: TextStyle(color: Utils.header)),
-                    onTap: () => launch("tel:+202 35345260"),
-                  ),
-                  ListTile(
-                    title: Text('Police',
-                        style: TextStyle(color: Utils.header)),
-                    onTap: () => launch("tel: 122"),
-                  ),
-                  ListTile(
-                    title: Text('New Cairo Police Station',
-                        style: TextStyle(color: Utils.header)),
-                    onTap: () => launch("tel:+202 26173630"),
-                  ),
-                  ListTile(
-                    title: Text('Fire Brigade – 180',
-                        style: TextStyle(color: Utils.header)),
-                    onTap: () => launch("tel:+202 23910115"),
-                  ),
-                  ListTile(
-                    title: Text('Road Rescue',
-                        style: TextStyle(color: Utils.header)),
-                    onTap: () => launch("tel:128"),
-                  ),
-                  ListTile(
-                    title: Text('Road Accident',
-                        style: TextStyle(color: Utils.header)),
-                    onTap: () => launch("tel:+201221110000"),
-                  )
-                ],
-              ),
-            ]),
-          ),
-        ));
+            body: Padding(
+              padding: EdgeInsets.fromLTRB(
+                  width * 0.02, height * 0.02, width * 0.03, height * 0.01),
+              child: TabBarView(children: [
+                new ListView(children: bupa),
+                new ListView(children: dell),
+                new ListView(children: local),
+              ]),
+            ),
+          ));
+    else {
+      return Scaffold(
+        body: ImageRotate(),
+      );
+    }
   }
 }
