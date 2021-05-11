aa5b1e56b91ea2b3ee6cbebda8a73950414d6fa4
welo10 <waleedehab99@gmail.com>
2021-05-11 14:58:39 +0200

    Loading indicator untill list of BUs are ready

--- lib/BusinessUnit.dart
+++ lib/BusinessUnit.dart
@@ -3,8 +3,11 @@ import 'package:connect_plus/widgets/Utils.dart';
 import 'package:connect_plus/services/web_api.dart';
 
 class BusinessUnit extends StatefulWidget {
-  BusinessUnit({Key key, this.PassValue}) : super(key: key);
+  BusinessUnit({Key key, this.PassValue, this.asyncCallController})
+      : super(key: key);
   final void Function(String value) PassValue;
+  final void Function(bool value) asyncCallController;
+
   @override
   _BusinessUnit createState() => _BusinessUnit();
 }
@@ -15,6 +18,9 @@ class _BusinessUnit extends State<BusinessUnit> {
   List<String> BUs = new List<String>();
   void _getBusinessUnits() async {
     BUs = await WebAPI.getBusinessUnits();
+    widget.asyncCallController(false);
+    dropdownValue = BUs[0];
+    print(dropdownValue);
     setState(() {
       Loaded = true;
     });
@@ -22,6 +28,8 @@ class _BusinessUnit extends State<BusinessUnit> {
   }
 
   void initState() {
+    WidgetsBinding.instance
+        .addPostFrameCallback((_) => {widget.asyncCallController(true)});
     _getBusinessUnits();
   }
 
@@ -29,7 +37,7 @@ class _BusinessUnit extends State<BusinessUnit> {
   Widget build(BuildContext context) {
     return Loaded
         ? DropdownButton<String>(
-            value: BUs[0],
+            value: dropdownValue,
             icon: const Icon(Icons.arrow_downward),
             iconSize: 24,
             elevation: 16,
