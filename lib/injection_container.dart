b3c89920cdb4bbe38ecc91a94ea1fba6989b981f
Ali-Amin <ali.m.amin98@gmail.com>
2021-05-20 14:17:48 +0200

    add push notifications dependency injection

--- lib/injection_container.dart
+++ lib/injection_container.dart
@@ -1,9 +1,12 @@
 import 'package:get_it/get_it.dart';
 import 'package:connect_plus/services/auth_service/injection_container.dart'
     as auth_service;
+import 'package:connect_plus/services/push_notifications_service/injection_container.dart'
+    as push_notifications;
 
 final GetIt sl = GetIt.instance;
 
 void init() {
   auth_service.init(sl);
+  push_notifications.init(sl);
 }
