3843b5240bece5864f7f7062fba9639565039ca0
Ali Mohamed <67805101+Alimohamad21@users.noreply.github.com>
2022-09-28 18:39:47 +0200

    ui improvements

--- lib/widgets/WireMagazineCard.dart
+++ lib/widgets/WireMagazineCard.dart
@@ -5,19 +5,23 @@ import 'package:flutter/cupertino.dart';
 import 'package:connect_plus/services/web_api.dart';
 import 'package:connect_plus/widgets/pdf_viewer_from_url.dart';
 
-
 class WireMagazineCard extends StatefulWidget {
-  const WireMagazineCard({Key key, @required this.wireMagazine,
+  const WireMagazineCard({
+    Key key,
+    @required this.wireMagazine,
   }) : super(key: key);
   final WireMagazine wireMagazine;
 
   @override
-  _WireMagazineCardState createState() => _WireMagazineCardState(this.wireMagazine);
+  _WireMagazineCardState createState() =>
+      _WireMagazineCardState(this.wireMagazine);
 }
 
 class _WireMagazineCardState extends State<WireMagazineCard> {
   final WireMagazine wireMagazine;
+
   _WireMagazineCardState(this.wireMagazine);
+
   Widget urlToImage(String imageUrl) {
     ImageCache _imageCache = PaintingBinding.instance.imageCache;
     if (_imageCache.currentSize >= 55 << 20 ||
@@ -27,7 +31,7 @@ class _WireMagazineCardState extends State<WireMagazineCard> {
     }
 
     return SizedBox(
-        width: MediaQuery.of(context).size.width*0.625,
+        width: MediaQuery.of(context).size.width * 0.75,
         height: MediaQuery.of(context).size.height *
             0.675, // otherwise the logo will be tiny
         child: FittedBox(
@@ -35,8 +39,8 @@ class _WireMagazineCardState extends State<WireMagazineCard> {
             child: CachedNetworkImage(
               filterQuality: FilterQuality.high,
               imageUrl: imageUrl,
-              memCacheWidth: (MediaQuery.of(context).size.width * 1).toInt(),
-              memCacheHeight: (MediaQuery.of(context).size.width * 0.5).toInt(),
+              memCacheWidth: (MediaQuery.of(context).size.width * 3).toInt(),
+              memCacheHeight: (MediaQuery.of(context).size.height * 3).toInt(),
             )));
   }
 
@@ -55,34 +59,32 @@ class _WireMagazineCardState extends State<WireMagazineCard> {
               color: Colors.black87,
               fontWeight: FontWeight.w500),
         ),
-        SizedBox(
-          height: 5,
-        ),
         Card(
             elevation: 2.0,
             clipBehavior: Clip.antiAlias,
-            margin: EdgeInsets.only(left:30.0,right:30.0,top:15,bottom: 30),
+            margin: EdgeInsets.only(
+                left: MediaQuery.of(context).size.width * 0.125,
+                right:MediaQuery.of(context).size.width * 0.125,
+                top: 30,
+                bottom: 30),
             shape: RoundedRectangleBorder(
                 borderRadius: BorderRadius.all(Radius.circular(10.0))),
             child: InkWell(
-              child: urlToImage(WebAPI.baseURL + wireMagazine.cover.url),
+                child: urlToImage(WebAPI.baseURL + wireMagazine.cover.url),
                 onTap: () async {
                   if (wireMagazine.pdf.url != null) {
                     String pathPDF = WebAPI.baseURL + wireMagazine.pdf.url;
                     Navigator.push(
                       context,
                       MaterialPageRoute<dynamic>(
-                        builder: (_) =>
-                            PDFViewerCachedFromUrl(
-                              url: pathPDF,
-                              title: "The Wire Edition ${wireMagazine.edition}",
-                            ),
+                        builder: (_) => PDFViewerCachedFromUrl(
+                          url: pathPDF,
+                          title: "The Wire Edition ${wireMagazine.edition}",
+                        ),
                       ),
                     );
                   }
-                }
-            )),
-
+                })),
       ],
     );
   }
